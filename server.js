0021096cbc4b7548740e8df46c6a0f5619f6b1ad
Yueh Chou <yueh.k.chou@gmail.com>
2017-03-31 19:51:31 -0400

    feat() server.js() added in Posse API Jsonx file

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
 const bodyParser = require('body-parser');
-
+const posseAPI = require('./server/posse_challenge.json');
 const app = express();
+const port = 5050;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -34,5 +35,12 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'client/index.html'))
 })
 
+// make API call at this address to get json file
+app.get('/api/posse', function(req, res) {
+  res.send(posseAPI);
+})
+
 // server listening at port
-app.listen(5050);
+app.listen(port, function() {
+  console.log('Port is listening at ', port)
+});
